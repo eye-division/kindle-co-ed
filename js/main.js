13ab0a3c56a3be5d59a2881ddb6a6d7cb4d58e3a
Alun Ward <a.ward@eyedivision.com>
2021-06-25 11:23:04 +0100

    Add expander and revise menus

--- js/main.js
+++ js/main.js
@@ -48,6 +48,11 @@
       touch: true
     });
 
+     // expander Bourbon
+    $('.expander-trigger').click(function(){
+      $(this).toggleClass("expander-hidden");
+    });
+    
   }); // end doc ready
 
 })(jQuery); // End of use strict
